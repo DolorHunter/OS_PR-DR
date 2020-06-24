bc0ed5dfcd4061b78d7b1f77f52195093ec4c08f
DolorHunter <DolorHunter@gmail.com>
2020-06-24 18:11:00 +0800

    Update main Add Resource Allocation Diagram File display

--- DeadlockRecovery/main.c
+++ DeadlockRecovery/main.c
@@ -3,10 +3,13 @@
 int main(){
     ResourceAllocation  rA;
     char fileName[30];
+    printf("Deadlock Recovery\n");
     initDeadlockRecovery(&rA);
     chooseFile(fileName);
     readFile(&rA, fileName);
-    printf("ANS = ");
+    printf("Resource Allocation Diagram File\n");
+    prtResourceAllocation(&rA);
+    printf("\nANS = ");
     deadlockRecovery(&rA);
     printf("\nANS1: 2013 / 2031\n");
     printf("ANS2: 41203 / 41230\n");
